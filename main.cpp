99aa5e237c2e1f9b53ced3c1039775a6114c6e57
cristinapereez <cperez58@mail.csuchico.edu>
2018-03-29 00:24:21 -0700

    First attempt at lab: reading from file

--- main.cpp
+++ main.cpp
@@ -1,14 +1,42 @@
-//Author:
+//Author: Cristina Perez
 #include <iostream>
 #include <fstream>
-
+#include <vector>
 using namespace std;
 
 int main()
 {
-  ifstream in("example.txt");
-  char first = ' ', last = ' ', current;
+ 
+char first;
+char last;
+/*
+fstream infile;
+vector<char> list;
 
+infile.open("example.txt");
+
+if( infile.is_open() )
+{
+
+while( !infile.eof() )
+{
+  infile>>
+*/
+  char letter;
+  ifstream in;
+  in.open("example.txt");
+  vector <char> list;
+  in>>letter;
+  list.push_back(letter);
+  
+  if( letter >= 'a' || letter <= 'z' || letter >= 'A' || letter <= 'Z')
+  {
+  for(int i = 0; i < list.size(); i++)
+  {
+  char first = list[i] ,
+  last = list[ list.size()-1] ,  current;
+  }
+  }
   //Read from the file opened by in, and the first letter (either upper or
   //lower case) should be assigned to first and the last letter (again in
   //either case) from the file to the variable last
